cd69757399c9aa3097be3b1c0ec0982bca30bc1a
Kevin Moens <kevmoens@yahoo.com>
2025-07-15 23:21:46 -0500

    pass the test name over when logging results

--- CSharpQuizBlazor/Models/QuizResultDto.cs
+++ CSharpQuizBlazor/Models/QuizResultDto.cs
@@ -3,7 +3,8 @@ namespace CSharpQuizBlazor.Models
     public class QuizResultDto
     {
         public string UserName { get; set; }
-        public int Score { get; set; }
+        public string Test { get; set; }
+		public int Score { get; set; }
         public List<string> WrongQuestions { get; set; }
     }
 }
